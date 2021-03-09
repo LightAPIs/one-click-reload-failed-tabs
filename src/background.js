3c237593bd419844ac36c7405ec3e080244796d5
Light <47911535+LightAPIs@users.noreply.github.com>
2021-03-09 17:13:09 +0800

    feat(all): implement basic functions

--- src/background.js
+++ src/background.js
@@ -1,21 +1,21 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.query(
+    {
+      status: 'complete',
+    },
+    result => {
+      if (!chrome.runtime.lastError && result.length > 0) {
+        result.forEach(tab => {
+          const { title, url, id } = tab;
+          if (url.includes(title)) {
+            chrome.tabs.reload(id, {
+              bypassCache: true,
+            });
+          }
+        });
+      }
+    }
+  );
 });
